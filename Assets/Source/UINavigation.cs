c0b836bd705365e7a212250e6aea79594fb519b1
Wafflefoot <68161191+Wafflefoot@users.noreply.github.com>
2020-07-11 19:40:25 -0500

    Able to close shop now

--- Assets/Source/UINavigation.cs
+++ Assets/Source/UINavigation.cs
@@ -14,7 +14,6 @@ public class UINavigation : MonoBehaviour
     {
         thisShopWindow = Instantiate(shopModal, contentPanel);
         Component[] Buttons = thisShopWindow.GetComponentsInChildren<Button>();
-<<<<<<< HEAD
         Buttons[0].GetComponent<Button>().onClick.AddListener(delegate { ResetGameState(); });
         Buttons[1].GetComponent<Button>().onClick.AddListener(delegate { CloseShopWindow(); });
         Buttons[2].GetComponent<Button>().onClick.AddListener(delegate { QuitGame(); });
@@ -33,8 +32,5 @@ public class UINavigation : MonoBehaviour
     void QuitGame()
     {
 
-=======
-        Debug.Log(Buttons[2].name);
->>>>>>> parent of 0ebfcc5... Revert "Merge branch 'master' of https://github.com/NewNakiruna/BirbGameJam"
     }
 }
