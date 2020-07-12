6c9784b930c3e97fc222e98fd4919df35b6916ea
Wafflefoot <68161191+Wafflefoot@users.noreply.github.com>
2020-07-11 19:14:11 -0500

    Current updates to ShopModal stuff

--- Assets/Source/UINavigation.cs
+++ Assets/Source/UINavigation.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UINavigation : MonoBehaviour
 {
     public Transform contentPanel;
     public GameObject shopModal;
 
+    protected GameObject thisShopWindow;
+
     public void OpenShopWindow()
     {
-        Instantiate(shopModal, contentPanel);
+        thisShopWindow = Instantiate(shopModal, contentPanel);
+        Component[] Buttons = thisShopWindow.GetComponentsInChildren<Button>();
+        Debug.Log(Buttons[2].name);
     }
 }
