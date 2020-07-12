e05041060aa62baeb0589b8a861dd328587b6535
Wafflefoot <68161191+Wafflefoot@users.noreply.github.com>
2020-07-11 20:57:37 -0500

    Updated UI Navigation to Build Shop from available Seeds in GardenManager

--- Assets/Source/ShopItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShopItem : MonoBehaviour
-{
-    public string Description;
-    public int Cost;
-}
--- Assets/Source/UINavigation.cs
+++ Assets/Source/UINavigation.cs
@@ -9,20 +9,35 @@ public class UINavigation : MonoBehaviour
     public Transform contentPanel;
     public GameObject shopModal;
     public GardenManager thisGardenManager;
+    public GameObject shopItem;
+
     protected GameObject thisShopWindow;
 
     public void OpenShopWindow()
     {
         thisShopWindow = Instantiate(shopModal, contentPanel);
         Component[] Buttons = thisShopWindow.GetComponentsInChildren<Button>();
+        
         Buttons[0].GetComponent<Button>().onClick.AddListener(delegate { ResetGameState(); });
         Buttons[1].GetComponent<Button>().onClick.AddListener(delegate { CloseShopWindow(); });
         Buttons[2].GetComponent<Button>().onClick.AddListener(delegate { QuitGame(); });
+
+        BuildTheShop();
     }
 
     void BuildTheShop()
     {
-            
+        Transform contentPanelForShop = thisShopWindow.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(0);
+        for (int i = 0;i<thisGardenManager.PossibleSeedTypes.Count;i++)
+        {
+            GameObject lineItem = Instantiate(shopItem,contentPanelForShop);
+            lineItem.transform.GetChild(0).transform.GetChild(2).GetComponent<Text>().text = thisGardenManager.PossibleSeedTypes[i].DisplayName;
+        }
+    }
+
+    void CreateShopItem()
+    {
+
     }
 
     void ResetGameState()
