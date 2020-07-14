21a80271ec4d2bff543d585738fcbdf1051a662b
Nakiruna <courtney.redlinger@gmail.com>
2020-07-14 17:39:37 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/NewNakiruna/BirbGameJam""
    This reverts commit a1237215d7c82d51e4503a80a5baf8a43bd39938.

--- Assets/Source/GardenManager.cs
+++ Assets/Source/GardenManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BirbSimulator
 {
@@ -37,6 +38,7 @@ namespace BirbSimulator
         public List<GardenVisitor> PossibleGardenVisitorPrefabs;
         public List<Seed> PossibleSeedTypes;
         public UINavigation NavigationManager;
+        public Text TextField;
         // End Inspector Values
 
         // Non-Inspector Values
@@ -177,6 +179,9 @@ namespace BirbSimulator
             {
                 feeder.UpdateFeeder(frameDeltaTime);
             }
+
+            //Update Money in real time
+            TextField.text = "$"+GetMoney().ToString();
         }
 
         void InitializeNewGame()
@@ -417,6 +422,10 @@ namespace BirbSimulator
         public void SetUIOpen(bool isOpen)
         {
             UIOpen = isOpen;
+        }
+        public bool IsUIOpen()
+        {
+            return UIOpen;
         }
 
         void SaveGame()
